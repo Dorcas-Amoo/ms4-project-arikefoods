6445b5ed32e0bf023d8e712e6813587bacf81236
Dorcas-Amoo <doorcars@gmail.com>
2020-10-19 16:43:05 +0000

    Added meta class in models.py and declared display in admin.py

--- menu/admin.py
+++ menu/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 from .models import Menu, Category
 
 # Register your models here.
-admin.site.register(Menu)
-admin.site.register(Category)
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+        'category',
+        'price',
+    )
+
+    ordering = ('category',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(Category, CategoryAdmin)
