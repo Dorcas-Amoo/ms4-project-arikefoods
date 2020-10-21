c503f025c26151ef5e80b8cd6bdc3cae1bb9b6cb
Dorcas-Amoo <doorcars@gmail.com>
2020-10-21 15:21:39 +0000

    Added order url to arikefoods/urls.py

--- arikefoods/urls.py
+++ arikefoods/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls' )),
+    path('', include('home.urls')),
     path('food_menu/', include('menu.urls')),
+    path('order/', include('order.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
