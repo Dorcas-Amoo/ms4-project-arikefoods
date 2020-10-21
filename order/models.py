b95c34c2a026243f8be2a8d98bb971aa82e9373e
Dorcas-Amoo <doorcars@gmail.com>
2020-10-21 15:27:53 +0000

    Order view, url and models setup and rendered in index.html

--- order/models.py
+++ order/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    """
+    This model stores each order details
+    """
+    discount = models.DecimalField(max_digits=6, decimal_places=2)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- order/views.py
+++ order/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def order(request):
+    """ A view to return the order page """
+
+    return render(request, 'order/order.html')
