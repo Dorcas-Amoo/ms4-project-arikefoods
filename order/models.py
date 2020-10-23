3adf3efad9f885ef6a06ad4dd32c38fd82852e00
Dorcas-Amoo <doorcars@gmail.com>
2020-10-23 14:01:33 +0000

    Created and migrated order models

--- order/models.py
+++ order/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from menu.models import Menu
 
 
 class Order(models.Model):
     """
     This model stores each order details
     """
+    menu = models.ForeignKey(Menu, null=True, blank=True, on_delete=models.SET_NULL)
     discount = models.DecimalField(max_digits=6, decimal_places=2)
     total = models.DecimalField(max_digits=6, decimal_places=2)
 
