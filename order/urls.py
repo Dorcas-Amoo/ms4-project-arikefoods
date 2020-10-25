0f5b3cf95a39b9fb5c8275ca210ce6e6beb33a09
Dorcas-Amoo <doorcars@gmail.com>
2020-10-25 00:38:58 +0000

    Created add_to_cart view and url

--- order/urls.py
+++ order/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
    path('order_cart/', views.view_order, name='view_order'),
+   path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
 ]
--- order/views.py
+++ order/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,20 @@ def view_order(request):
     """ A view to return the order_cart page """
 
     return render(request, 'order/order.html')
+
+
+def add_to_cart(request, item_id):
+    """ A view to add item's quantity to the order_cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
