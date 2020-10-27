619b54598fa80dab91e1226029f7cbccecd5cea0
Dorcas-Amoo <doorcars@gmail.com>
2020-10-27 01:47:55 +0000

    Created templatetags folder and added cal subtotal template

---
+++ order/templatetags/cart_tools.py
@@ -0,0 +1,10 @@
+from django import template
+
+""" The code below was generated from Code Institute's Boutique Ad Tutorial """
+
+register = template.Library()
+
+
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
+    return price * quantity
