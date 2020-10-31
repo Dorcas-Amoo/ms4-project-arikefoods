e18eb23e31e38399185b1a9e41fc12bbc3ce8d4d
Dorcas-Amoo <doorcars@gmail.com>
2020-10-31 10:20:06 +0000

    Expanded checkout views and created checkout_success logic

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import OrderLineItem
+from menu.models import Menu
 from order.contexts import cart_contents
 
 import stripe
@@ -11,6 +12,40 @@ import stripe
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    if request.method == 'POST':
+        cart = request.session.get('order', {})
+
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'phone_number': request.POST['phone_number'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'town_or_city': request.POST['town_or_city'],
+            'county': request.POST['county'],
+            'postcode': request.POST['postcode'],
+            'country': request.POST['country'],
+        }
+        order_form = OrderForm(form_data)
+
+        order = order_form.save()
+        for item_id, item_data in cart.items():
+            menu = Menu.objects.get(id=item_id)
+
+            order_line_item = OrderLineItem(
+                order=order,
+                menu=menu,
+                quantity=item_data,
+            )
+            order_line_item.save()
+
+        # Save the info to the user's profile if all is well
+        request.session['save_info'] = 'save-info' in request.POST
+        return redirect(reverse('checkout_success'))
+
+    else:
+        stripe_public_key = settings.STRIPE_PUBLIC_KEY
+        stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     cart = request.session.get('order', {})
     if not cart:
@@ -28,9 +63,21 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
 
     return render(request, template, context)
+
+
+def checkout_success(request):
+    """
+    Handles successful checkout. Checks that cart's content
+    is still present and deletes it,
+    then renders the checkout success page.
+    """
+    if 'order' in request.session:
+        del request.session['order']
+
+    return render(request, 'checkout/checkout_success.html')
